a5b1f604dd1c2e079ee176f653193501b2b06f80
hexbitCTF <hexabit777@gmail.com>
2026-05-07 21:37:04 +0000

    Auto-sync: Thu May  7 21:37:04 UTC 2026

--- config.def.h
+++ config.def.h
@@ -13,10 +13,10 @@ static const char *fonts[] = {
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-    /* fg         bg       */
-    [SchemeNorm] = { "#bbbbbb", "#222222" },
-    [SchemeSel] = { "#eeeeee", "#005577" },
-    [SchemeOut] = { "#000000", "#00ffff" },
+    /*     fg         bg       */
+    [SchemeNorm] = { "#c0caf5", "#24283b" }, // Normal: Silver text on Storm background
+    [SchemeSel]  = { "#24283b", "#7aa2f7" }, // Selected: Storm text on Blue background
+    [SchemeOut]  = { "#000000", "#00ffff" }, // Out: Default (rarely seen)
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 10;
